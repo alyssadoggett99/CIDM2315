37ca852a2b3af3e1649771122efc68e406aca0a2
alyssadoggett99 <123403074+alyssadoggett99@users.noreply.github.com>
2023-01-23 13:58:07 -0600

    Upload Homework1

---
+++ Homework1.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        float X = 2.5f ;
+        float Y = 3.3f ;
+
+        float Z = (4*X*X)+3*Y ;
+        
+        Console.Write("X = " +X);
+        Console.Write(", ");
+
+        Console.WriteLine("Y = " +Y);
+
+        Console.Write("The value of Z is: " +Z);
+    }
+}
