53965fd4287d62f8a909d2500923c4ed7f484625
alyssadoggett99 <123403074+alyssadoggett99@users.noreply.github.com>
2023-02-08 20:19:46 -0700

    Homework4 Question 1

---
+++ Homework4Q1.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework4Q1;
+class Program
+{
+    static int largest_num(int a, int b){
+        if(a>b){
+            return a;
+        }
+        else if(b>a){
+            return b;
+        }
+        else{
+            return -1;
+        }
+    }
+    static void Main() {
+        int a = 3, b = 5;
+        int largest = largest_num(a, b);
+        
+    Console.WriteLine($"a = {a}; b={b} \nThe largest number is: " + largest);
+    }
+}
