4633d0d8b2d11b0dd818d4e639ec7d6542ae9018
alyssadoggett99 <123403074+alyssadoggett99@users.noreply.github.com>
2023-02-15 17:28:03 -0700

    Homework 5 Question 2

---
+++ Homework5Q2.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework5Q2;
+class Program
+{
+        static int largest(int a, int b){
+        if (a>b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        } }
+    static int largest(int a, int b, int c, int d)
+    {
+        return largest(largest(a, b), largest(c, d));}
+    static void Main(){
+        int a = 3, b = 5, c = 7, d = 9;
+        int largest_num = largest(a, b, c, d);
+       
+    Console.WriteLine($"a = {a}; b={b}; c = {c}; d={d}; \nThe largest number is: " + largest_num);}
+}
