f630e65f8c12400707b7cc986325050bb72f1804
alyssadoggett99 <123403074+alyssadoggett99@users.noreply.github.com>
2023-02-15 20:25:22 -0700

    Update Homework5Q2.cs

--- Homework5Q2.cs
+++ Homework5Q2.cs
@@ -1,21 +1,23 @@
-﻿namespace Homework5Q2;
+namespace Homework5Q2;
 class Program
 {
-        static int largest(int a, int b){
-        if (a>b)
-        {
+ static int largest(int a, int b){
+        if (a>b){
             return a;
         }
         else
         {
-            return b;
-        } }
-    static int largest(int a, int b, int c, int d)
+            return b;}
+ }
+    static int maxOfFour(int a, int b, int c, int d)
     {
-        return largest(largest(a, b), largest(c, d));}
+        int max1 = largest(a, b);
+        int max2 = largest(c, d);
+        int result = largest(max1, max2);
+        return result;}
     static void Main(){
         int a = 3, b = 5, c = 7, d = 9;
-        int largest_num = largest(a, b, c, d);
-       
+        int largest_num = maxOfFour(a, b, c, d);
+
     Console.WriteLine($"a = {a}; b={b}; c = {c}; d={d}; \nThe largest number is: " + largest_num);}
 }
